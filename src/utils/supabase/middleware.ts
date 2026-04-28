e05c34569ab5e470d41c2a4c0d6928c30f1c66ee
Marcos <corretormarcosalmeida@gmail.com>
2026-04-28 14:23:05 -0300

    fix: definir variavel supabase no middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -6,7 +6,7 @@ export async function updateSession(request: NextRequest) {
     request,
   })
 
-  createServerClient(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
